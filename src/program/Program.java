b568a4ebc37c8399112c758f1b3ccd113b943eff
Florian Schmidt <florian.schmidt1995@gmx.de>
2017-03-07 09:13:50 +0100

    ADDED class Program

---
+++ src/program/Program.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package program;
+
+/**
+ * @author Florian
+ *
+ */
+public class Program {
+
+	/**
+	 * 
+	 */
+	public Program() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
