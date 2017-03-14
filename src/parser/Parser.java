d652316dca24f46d66095d92213a941aafcd841b
z003hj3z <z003hj3z@MD1K48SC.ad001.siemens.net>
2017-03-14 10:00:47 +0100

    parser3

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -61,7 +61,42 @@ public class Parser {
 		expect(DslScanner.Tokens.SEMICOLON);
 		mScanner.consume();
 	}
+	private void parseDef(){
+		expect(DslScanner.Tokens.DEF);
+		mScanner.consume();
+		expect(DslScanner.Tokens.ID);
+		mScanner.consume();
+		parseOptParam();
+		while(mScanner.la()==DslScanner.Tokens.ID||mScanner.la()==DslScanner.Tokens.DEF){
+			parseStat();
+		}
+		expect(DslScanner.Tokens.END);
+		mScanner.consume();
+	}
+	private void parseArgList(){
+		while(mScanner.la()==DslScanner.Tokens.MINUS||mScanner.la()==DslScanner.Tokens.ID||mScanner.la()==DslScanner.Tokens.NUM){
+			parseExpr();
+		}
+	}
+	private void parseExpr(){
+		parseUnary();
+		while(mScanner.la()==DslScanner.Tokens.PLUS||mScanner.la()==DslScanner.Tokens.MINUS||mScanner.la()==DslScanner.Tokens.MULT||mScanner.la()==DslScanner.Tokens.DIV){
+			parseOP();
+			parseUnary();
+		}
+	}
+	private void parseUnary(){
+		if(mScanner.la()==DslScanner.Tokens.MINUS){
+			mScanner.consume();
+		}
+		parseTerm();
+	}
 	
+	private void parseTerm(){
+		if(mScanner.la()==DslScanner.Tokens.ID||mScanner.la()==DslScanner.Tokens.NUM){
+			mScanner.consume();
+		}
+	}
 
 	void expect(DslScanner.Tokens token){
 		if (mScanner.la()!=token){
