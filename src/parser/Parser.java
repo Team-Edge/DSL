bd7d872d82b32a1f571dbf06959db98a12667df5
z003hj3z <z003hj3z@MD1K48SC.ad001.siemens.net>
2017-03-14 09:38:33 +0100

    parser1

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -18,15 +18,46 @@ public class Parser {
 	 */
 	public Parser(DslScanner scanner) {
 		// TODO Auto-generated constructor stub
-		
+		mScanner = scanner;
+		mScanner.consume();
 	}
 	
 	public void ParseShape()
 	{
+		expect(DslScanner.Tokens.SHAPE);
+		mScanner.consume();
+		expect(DslScanner.Tokens.ID);
+		mScanner.consume();
+		expect(DslScanner.Tokens.SIZE);
+		mScanner.consume();
+		expect(DslScanner.Tokens.NUM);
+		mScanner.consume();
+		expect(DslScanner.Tokens.NUM);
+		mScanner.consume();
+		expect(DslScanner.Tokens.SEMICOLON);
+		mScanner.consume();
+		while (mScanner.la()==DslScanner.Tokens.ID||mScanner.la()==DslScanner.Tokens.DEF){
+			parseStat();
+		}
+		expect(DslScanner.Tokens.END);
+		mScanner.consume();
+
+			
+	}
+	
+	private void parseStat(){
 		
 	}
 	
-	//Kommentar
+	void expect(DslScanner.Tokens token){
+		if (mScanner.la()!=token){
+			System.out.println("Error");
+			System.exit(1);
+		}
+		
+		
+	}
+	
 	
 
 }
